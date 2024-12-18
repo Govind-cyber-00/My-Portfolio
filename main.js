866ccef07ca20ba4c6b20a8e4c1b2c6e9687baee
Govind <govindyadav46324@gmail.com>
2024-12-18 18:02:30 +0530

    navbar

--- main.js
+++ main.js
@@ -12,3 +12,19 @@ document.querySelector('.menu-toggle').addEventListener('click', () => {
 // document.querySelectorAll('.navbar i').forEach(icon =>{
 //     icon.computedStyleMap.display ='none';
 // });
+
+
+// Navbar activation steps
+
+const navLinks = document.querySelectorAll('.nav-link');
+
+// Har link pe click event listener lagao
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    // Pehle se active link ko hatao
+    document.querySelector('.nav-link.active')?.classList.remove('active');
+    
+    // Current clicked link ko active class do
+    link.classList.add('active');
+  });
+});
