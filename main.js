0df4df8e743e092b142c781c9884a06ae6d81269
Govind-cyber-00 <govindyadav46324@gmail.com>
2025-01-10 19:47:43 +0530

    skills sections

--- main.js
+++ main.js
@@ -28,3 +28,20 @@ navLinks.forEach(link => {
     link.classList.add('active');
   });
 });
+
+
+// JavaScript for skill box hover effect
+const skillBoxes = document.querySelectorAll('.skill-box-a','skill-box-b','skill-box-c','skill-box-d','skill-box-e','skill-box-f','skill-box-g','skill-box-h','skill-box-i','skill-box-j','skill-box-k');
+
+skillBoxes.forEach(skillBox => {
+  skillBox.addEventListener('mouseenter', () => {
+    skillBox.style.transform = 'translateY(-5px)';
+  });
+  
+  skillBox.addEventListener('mouseleave', () => {
+    skillBox.style.transform = 'translateY(0)';
+  });
+});
+
+
+
